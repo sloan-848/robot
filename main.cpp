a61046e0803dbe33284466634fe7e091c2b7e49a
Will Sloan <sloan.848@osu.edu>
2014-03-20 17:17:12 -0400

    Ready for PT 3/20

--- main.cpp
+++ main.cpp
@@ -29,12 +29,6 @@ int main(void)
         Sleep(2.0);
     }
 
-  /*LCD.WriteLine("Waiting for light");
-    while(!hal.cdsReady(START));
-    LCD.WriteLine("Lets do it.");
-    Sleep(1.0);
-    */
-
     LCD.Clear(CLEARCOLOR);
 
     LCD.WriteLine("Flipping Switch");
@@ -54,25 +48,31 @@ int main(void)
     if(hal.cdsColor() == hal.REDLIGHT){
         LCD.WriteLine("Red");
         Sleep(1.0);
-        hal.turnLeft(40);
+        hal.turnLeft(35);
         Sleep(1.0);
 
         LCD.WriteLine("Backing up to drop");
-        hal.moveBackward(5,55);
+        hal.moveBackward(10,50);
+
+        LCD.WriteLine("Turning to match up with drop point");
+        hal.turnRight(35);
+
+        LCD.WriteLine("Back up for drop.");
+        hal.timeBack(3,50);
 
         Sleep(2.0);
 
-        LCD.WriteLine("Moving Forward");
-        hal.moveForward(5,45);
+        LCD.WriteLine("Move Forward");
+        hal.moveForward(4,50);
 
-        LCD.WriteLine("Turning Left");
-        hal.turnLeft(30);
+        LCD.WriteLine("Turning Right");
+        hal.turnLeft(20);
 
         LCD.WriteLine("Going Forward");
-        hal.moveForward(5,45);
+        hal.moveForward(12,50);
 
         LCD.WriteLine("Turn to go forward up ramp.");
-        hal.turnRight(30);
+        hal.turnRight(15);
     }
     else if(hal.cdsColor() == hal.BLUELIGHT){
         LCD.WriteLine("Blue");
@@ -81,13 +81,13 @@ int main(void)
         Sleep(1.0);
 
         LCD.WriteLine("Backing up to drop");
-        hal.moveBackward(8,50);
+        hal.moveBackward(10,50);
 
         LCD.WriteLine("Turning to match up with drop point");
         hal.turnLeft(35);
 
         LCD.WriteLine("Back up for drop.");
-        hal.timeBack(5,50);
+        hal.timeBack(3,50);
 
         Sleep(2.0);
 
@@ -95,7 +95,7 @@ int main(void)
         hal.moveForward(4,50);
 
         LCD.WriteLine("Turning Right");
-        hal.turnRight(15);
+        hal.turnRight(20);
 
         LCD.WriteLine("Going Forward");
         hal.moveForward(12,50);
@@ -109,55 +109,15 @@ int main(void)
     }
 
     LCD.WriteLine("Go forward up ramp.");
-    hal.moveForward(28,70);
+    hal.moveForward(25,70);
 
     LCD.WriteLine("Turning Left");
     hal.turnLeft(90);
 
     LCD.WriteLine("To the charger!");
-    hal.moveForward(40,55);
+    hal.moveForward(40,60);
 
-/*
-    Sleep(.5);
 
-    LCD.WriteLine("Lowering Arm.");
-    hal.setArmAngle(20,8);
-
-    Sleep(.5);
-
-    LCD.WriteLine("Moving into position");
-    hal.moveForward(15);
-
-    Sleep(.5);
-
-    LCD.WriteLine("Raising Arm.");
-    hal.setArmAngle(20,22);
-
-    Sleep(.5);
-
-    LCD.WriteLine("Moving backwards");
-    hal.moveBackward(15);
-
-    Sleep(.5);
-
-    LCD.WriteLine("Turn Left");
-    hal.turnLeft(2300);
-
-    Sleep(.5);
-
-    LCD.WriteLine("Move Down Ramp.");
-    hal.moveBackward(35);
-
-    Sleep(.5);
-
-    LCD.WriteLine("Turn Left.");
-    hal.turnLeft(1300);
-
-    Sleep(.5);
-
-    LCD.WriteLine("Move into chiller");
-    hal.moveForward(8);
-*/
     return 1;
 }
 
